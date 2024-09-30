a695f1eff4657d7d74c4982bf3e235d3c522e929
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-09-30 15:57:37 +0200

    Primer ejercicio javascript

---
+++ 001_Probando/js/programa.js
@@ -0,0 +1,12 @@
+// Escribir directamente en el documento:
+// document.write("hola mundo");
+// Mostrar algo en la consola:
+console.log("hola mundo");
+/*
+Una alerta:
+Una alerta es código bloqueante porque javascript es un lenguaje monohilo.
+*/
+// alert("hola mundo");
+
+let x = "mundo";
+document.writeln("hola " + x);
