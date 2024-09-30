d6ea0bd2378deb3b0bc2a15c34b9aaaf12b92b5b
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-09-30 16:12:13 +0200

    Dado pero con letras, sacando la posicion de un string

--- 003_Dado_Letras/js/script.js
+++ 003_Dado_Letras/js/script.js
@@ -1,6 +1,6 @@
 function lanzarDado() {
 
-    const abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-    document.querySelector("h1").innerText = abecedario[Math.ceil(Math.random() * abecedario.length) - 1]
+    const abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    document.querySelector("h1").innerText = abecedario.charAt(Math.ceil(Math.random() * abecedario.length) - 1);
 
 }
