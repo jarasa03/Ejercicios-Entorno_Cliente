7628d79b4c347b4f2df7a816e6ff03bd5b122ff1
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-03 17:49:25 +0200

    Calculadora de potencias con BigInt

---
+++ 009_Potencias_Big_Int/js/script.js
@@ -0,0 +1,15 @@
+function calcularPotencia() {
+
+    event.preventDefault();
+    let resultado3 = 1;
+    let base = document.getElementById("base").value;
+    let exponente = document.getElementById("exponente").value;
+
+    let baseOK = BigInt(base);
+    let expoOK = BigInt(exponente);
+
+    resultado3 = baseOK ** expoOK;
+
+    document.getElementById("resul3").innerHTML = "Resultado de " + baseOK + "<sup>" + expoOK + "</sup>" + " es igual a " + resultado3;
+
+}
