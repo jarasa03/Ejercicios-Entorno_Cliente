a5d1e38c2468287b83da3f238fc196d428deea8b
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-03 17:49:51 +0200

    Calculadora de potencias y de longitud de resultado

---
+++ 010_Potencias_Length/js/script.js
@@ -0,0 +1,17 @@
+function calcularPotencia() {
+
+    event.preventDefault();
+    let resultado3 = 1;
+    let base = document.getElementById("base").value;
+    let exponente = document.getElementById("exponente").value;
+
+    let baseOK = BigInt(base);
+    let expoOK = BigInt(exponente);
+
+    resultado3 = baseOK ** expoOK;
+    let resultado3String = String(resultado3);
+
+    document.getElementById("resul3").innerHTML = "Resultado de " + baseOK + "<sup>" + expoOK + "</sup>" + " es igual a " + resultado3;
+    document.getElementById("tamanyo").innerHTML = "Tamaño: " + resultado3String.length;
+
+}
