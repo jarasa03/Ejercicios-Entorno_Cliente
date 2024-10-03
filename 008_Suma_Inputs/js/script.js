74065bd05f39856c428a9422dbbbe98abf801acb
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-03 17:49:01 +0200

    Suma de dos inputs

---
+++ 008_Suma_Inputs/js/script.js
@@ -0,0 +1,15 @@
+function suma() {
+
+    event.preventDefault();
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+
+    let valor1OK = Number(valor1);
+    let valor2OK = Number(valor2);
+
+
+    let resultado = valor1OK + valor2OK;
+
+    document.getElementById("escribir").innerText = "La suma da " + resultado;
+}
