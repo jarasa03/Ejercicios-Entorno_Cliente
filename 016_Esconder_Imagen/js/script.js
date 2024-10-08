ad923a664dd107807739cadc0aab4f2232b27bc1
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-08 16:45:59 +0200

    Imagen que desaparece y aparece con un boton

---
+++ 016_Esconder_Imagen/js/script.js
@@ -0,0 +1,7 @@
+function fueraBorracho() {
+    document.getElementById("gragas").style.visibility = "hidden";
+}
+
+function holaBorracho() {
+    document.getElementById("gragas").style.visibility = "visible";
+}
