a5dab1796979e1b1c95dc09985f19f5512e1feca
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-28 16:37:57 +0100

    Tratamiento de fechas

---
+++ 028_Nombre_Dia_Mes/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("fecha").addEventListener("input", () => {
+    let fecha = document.getElementById("fecha").value;
+    const seleccion = new Date(fecha);
+
+    let anyo = seleccion.getFullYear();
+    let mes = seleccion.getMonth();
+    let dia = seleccion.getDate();
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    let diaPalabra = diasSemana[seleccion.getDay()];
+    let mesPalabra = meses[mes];
+
+    document.querySelector("h1").innerText = "El " + dia + " de " + mesPalabra + " del " + anyo + " es " + diaPalabra;
+});
