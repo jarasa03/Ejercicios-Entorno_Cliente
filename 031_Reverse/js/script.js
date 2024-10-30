1afaa5c2126bb1f24490f3bc4535d026b53c675a
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-30 18:00:55 +0100

    Revertir un String

---
+++ 031_Reverse/js/script.js
@@ -0,0 +1,17 @@
+function reverse(revertir) {
+    return revertir.split("").reverse().join(""); // Invertir caracteres, no palabras
+};
+
+document.getElementById("botoncito").addEventListener("click", () => {
+    // Obtiene el valor actual, lo invierte y lo establece de nuevo en el campo
+    const textoActual = document.getElementById("escribir").value;
+    document.getElementById("escribir").value = reverse(textoActual);
+});
+
+// function reverse2(revertir) {
+//     let palabra = "";
+//     for (let i = (revertir.length - 1); i <= 0; i--) {
+//         palabra += revertir[i];
+//     }
+//     return palabra; // Invertir caracteres, no palabras
+// };
