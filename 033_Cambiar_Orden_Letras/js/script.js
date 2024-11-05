3d94f878c4a200f8882396fa53290bf1ba28c3a2
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-11-05 15:53:55 +0100

    Cambiar el orden de dos letras de un string

---
+++ 033_Cambiar_Orden_Letras/js/script.js
@@ -0,0 +1,19 @@
+function swap(eliminar, n, m) {
+    const array = eliminar.split(""); // Convertir la cadena en un array
+    let string = ""; // String que almacenará lo que debemos imprimir
+
+    let temp = array[n];
+    array[n] = array[m];
+    array[m] = temp;
+
+    for (let i = 0; i < array.length; i++) {
+        string += array[i];
+    }
+
+    return string;
+}
+
+document.getElementById("botoncito").addEventListener("click", () => {
+    const textoActual = document.getElementById("escribir").value;
+    document.getElementById("escribir").value = swap(textoActual, 0, 5);
+});
