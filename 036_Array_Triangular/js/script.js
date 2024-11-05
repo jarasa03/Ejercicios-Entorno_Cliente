76968d5024707e4524edeeb4f07de9a94a684c11
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-11-05 16:32:26 +0100

    Método para crear un array de arrays de tamaño dado por parámetro

---
+++ 036_Array_Triangular/js/script.js
@@ -0,0 +1,28 @@
+function showArray(array) {
+    let string = "";
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            string += array[i].join(" ") + "\n";
+        } else {
+            string += array[i] + " ";
+        }
+    }
+    return string;
+}
+
+function creaArrayTriangular(filas) {
+    let arrayTriangular = [];
+
+    for (let i = 0; i < filas; i++) {
+        let arrayAuxiliar = []; // Crear un nuevo array auxiliar para cada fila
+        for (let j = 0; j <= i; j++) {
+            arrayAuxiliar[j] = j + 1; // Asignar el valor a cada elemento de la fila
+        }
+        arrayTriangular[i] = arrayAuxiliar; // Añadir el array auxiliar al array triangular
+    }
+
+    return arrayTriangular;
+}
+
+let x = creaArrayTriangular(4);
+document.getElementById("introducir").innerText = showArray(x);
